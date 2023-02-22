f6decd0fbd35e569f8525935abb283bb5008fc97
IsakGairatov <97949548+IsakGairatov@users.noreply.github.com>
2023-02-22 20:35:52 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-secztu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-secztu)
---
+++ src/app/products.ts
@@ -0,0 +1,99 @@
+export interface Product {
+  id: number;
+  img: string;
+  rating: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    img: "",
+    rating: 0,
+    name: '',
+    price: 0,
+    description: 'dsds'
+  },
+  {
+    id: 2,
+    img: "",
+    rating: 0,
+    name: '',
+    price: 0,
+    description: ''
+  },
+  {
+    id: 3,
+    img: "",
+    rating: 0,
+    name: '',
+    price: 0,
+    description: ''
+  },
+  {
+    id: 4,
+    img: "",
+    rating: 0,
+    name: '',
+    price: 0,
+    description: ''
+  },
+  {
+    id: 5,
+    img: "",
+    rating: 0,
+    name: '',
+    price: 0,
+    description: ''
+  },
+  {
+    id: 6,
+    img: "",
+    rating: 0,
+    name: '',
+    price: 0,
+    description: ''
+  },
+  {
+    id: 7,
+    img: "",
+    rating: 0,
+    name: '',
+    price: 0,
+    description: ''
+  },
+  {
+    id: 8,
+    img: "",
+    rating: 0,
+    name: '',
+    price: 0,
+    description: ''
+  },
+  {
+    id: 9,
+    img: "",
+    rating: 0,
+    name: '',
+    price: 0,
+    description: ''
+  },
+  {
+    id: 10,
+    img: "",
+    rating: 0,
+    name: '',
+    price: 0,
+    description: ''
+  }
+  
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
